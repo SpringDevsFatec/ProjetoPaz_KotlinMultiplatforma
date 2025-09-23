8bf28df0143599dba9b406bea186d6da137ca250
João Victor Alexandre da Silva <joaosilva.dev01@gmail.com>
2025-09-22 23:17:36 -0300

    Add files via upload

---
+++ backend/projeto-paz-kotlin/src/main/kotlin/com/projetopaz/kotlin/ProjetoPazKotlinApplication.kt
@@ -0,0 +1,11 @@
+package com.projetopaz.kotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ProjetoPazKotlinApplication
+
+fun main(args: Array<String>) {
+	runApplication<ProjetoPazKotlinApplication>(*args)
+}
---
+++ backend/projeto-paz-kotlin/src/test/kotlin/com/projetopaz/kotlin/ProjetoPazKotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.projetopaz.kotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ProjetoPazKotlinApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
